d5e56f613b3b8dc9cfb2a6e00c040ed0da1342e8
Aaron Weeden <aweeden@shodor.org>
2018-05-13 15:23:44 -0500

    Refactor to simplify

--- life.c
+++ life.c
@@ -79,11 +79,11 @@ void exit_if(int boolean_expression, char function_name[32], int OUR_RANK)
  ****************/
 int main(int argc, char **argv)
 {
-  int NUMBER_OF_ROWS = 5, NUMBER_OF_COLUMNS = 5, NUMBER_OF_TIME_STEPS = 5, 
-      current_row, current_column, neighbor_row, neighbor_column,
-      number_of_alive_neighbors, c, return_value; 
+  int NUM_ROWS = 5, NUM_COLS = 5, NUM_STEPS = 5, 
+      row, col, neighbor_row, neighbor_column,
+      num_alive_neighbors, c, return_value; 
   int **current_grid, **next_grid;
-  int current_time_step;
+  int step;
 
   /* Parse command line arguments */ 
   while((c = getopt(argc, argv, "r:c:t:")) != -1)
@@ -91,17 +91,17 @@ int main(int argc, char **argv)
     switch(c)
     {
       case 'r':
-        NUMBER_OF_ROWS = atoi(optarg);
+        NUM_ROWS = atoi(optarg);
         break;
       case 'c':
-        NUMBER_OF_COLUMNS = atoi(optarg);
+        NUM_COLS = atoi(optarg);
         break;
       case 't':
-        NUMBER_OF_TIME_STEPS = atoi(optarg);
+        NUM_STEPS = atoi(optarg);
         break;
       case '?':
       default:
-        fprintf(stderr, "Usage: %s [-r NUMBER_OF_ROWS] [-c NUMBER_OF_COLUMNS] [-t NUMBER_OF_TIME_STEPS]\n", argv[0]);
+        fprintf(stderr, "Usage: %s [-r NUM_ROWS] [-c NUM_COLS] [-t NUM_STEPS]\n", argv[0]);
         exit(-1);
     }
   }
@@ -109,128 +109,106 @@ int main(int argc, char **argv)
   argv += optind;
 
   /* Make sure we have enough rows, columns, and time steps */
-  return_value = assert_minimum_value("row", NUMBER_OF_ROWS, MINIMUM_ROWS);
-  return_value += assert_minimum_value("column", NUMBER_OF_COLUMNS,
+  return_value = assert_minimum_value("row", NUM_ROWS, MINIMUM_ROWS);
+  return_value += assert_minimum_value("column", NUM_COLS,
       MINIMUM_COLUMNS);
-  return_value += assert_minimum_value("time step", NUMBER_OF_TIME_STEPS,
+  return_value += assert_minimum_value("time step", NUM_STEPS,
       MINIMUM_TIME_STEPS);
 
   /* Exit if we don't */
   if(return_value != 0)
+  {
     exit(-1);
+  }
 
   /* Allocate enough space in the current grid and next grid for the
    *  number of rows and the number of columns, plus the ghost rows
    *  and columns */
-  exit_if(((current_grid = (int**)malloc((NUMBER_OF_ROWS + 2) 
-            * (NUMBER_OF_COLUMNS + 2) * sizeof(int))) == NULL),
+  exit_if(((current_grid = (int**)malloc((NUM_ROWS + 2) 
+            * (NUM_COLS + 2) * sizeof(int))) == NULL),
       "malloc(current_grid)", 0);
-  exit_if(((next_grid = (int**)malloc((NUMBER_OF_ROWS + 2) 
-            * (NUMBER_OF_COLUMNS + 2) * sizeof(int))) == NULL),
+  exit_if(((next_grid = (int**)malloc((NUM_ROWS + 2) 
+            * (NUM_COLS + 2) * sizeof(int))) == NULL),
       "malloc(next_grid)", 0);
-  for(current_row = 0; current_row <= NUMBER_OF_ROWS + 1;
-      current_row++)
+  for(row = 0; row <= NUM_ROWS + 1; row++)
   {
-    exit_if(((current_grid[current_row]
-            = (int*)malloc((NUMBER_OF_COLUMNS + 2) * sizeof(int))) 
+    exit_if(((current_grid[row]
+            = (int*)malloc((NUM_COLS + 2) * sizeof(int))) 
           == NULL), "malloc(current_grid[some_row])", 0);
-    exit_if(((next_grid[current_row]
-            = (int*)malloc((NUMBER_OF_COLUMNS + 2) * sizeof(int)))
+    exit_if(((next_grid[row]
+            = (int*)malloc((NUM_COLS + 2) * sizeof(int)))
           == NULL), "malloc(next_grid[some_row])", 0);
   }
 
   srandom(time(NULL));
 
   /* Initialize the grid (each cell gets a random state) */
-  for(current_row = 1; current_row <= NUMBER_OF_ROWS;
-      current_row++)
+  for(row = 1; row <= NUM_ROWS; row++)
   {
-#pragma omp parallel for private(current_column)
-    for(current_column = 1; current_column <= NUMBER_OF_COLUMNS;
-        current_column++)
+#pragma omp parallel for private(col)
+    for(col = 1; col <= NUM_COLS; col++)
     {
-      current_grid[current_row][current_column] =
-        random() % (ALIVE + 1);
+      current_grid[row][col] = random() % (ALIVE + 1);
     }
   }
 
-  /* Determine the process with the next-lowest rank */
-
-  /* Determine the process with the next-highest rank */
-
   /* Run the simulation for the specified number of time steps */
-  for(current_time_step = 0; current_time_step <= NUMBER_OF_TIME_STEPS - 1;
-      current_time_step++)
+  for(step = 0; step <= NUM_STEPS - 1; step++)
   {
-#pragma omp parallel private(current_column)
-    for(current_column = 0;
-        current_column <= NUMBER_OF_COLUMNS + 1;
-        current_column++)
+#pragma omp parallel private(col)
+    for(col = 0; col <= NUM_COLS + 1; col++)
     {
       /* Set the top row to be the same as the second-to-last row */
-      current_grid[0][current_column]
-        = current_grid[NUMBER_OF_ROWS][current_column];
+      current_grid[0][col] = current_grid[NUM_ROWS][col];
 
       /* Set the bottom row to be the same as the second-to-top row */
-      current_grid[NUMBER_OF_ROWS + 1][current_column]
-        = current_grid[1][current_column];
+      current_grid[NUM_ROWS + 1][col] = current_grid[1][col];
     }
 
     /* Set up the ghost columns */
-    for(current_row = 0; current_row <= NUMBER_OF_ROWS + 1;
-        current_row++)
+    for(row = 0; row <= NUM_ROWS + 1; row++)
     {
       /* The left ghost column is the same as the farthest-right, non-ghost
          column */
-      current_grid[current_row][0] =
-        current_grid[current_row][NUMBER_OF_COLUMNS];
+      current_grid[row][0] = current_grid[row][NUM_COLS];
 
       /* The right ghost column is the same as the farthest-left, non-ghost
          column */
-      current_grid[current_row][NUMBER_OF_COLUMNS + 1] =
-        current_grid[current_row][1];
+      current_grid[row][NUM_COLS + 1] = current_grid[row][1];
     }
 
     /* Display the current grid */
-    printf("Time Step %d:\n", current_time_step);
-    for(current_row = 0; current_row <= NUMBER_OF_ROWS + 1;
-        current_row++)
+    printf("Time Step %d:\n", step);
+    for(row = 0; row <= NUM_ROWS + 1; row++)
     {
-      if(current_row == 1)
+      if(row == 1)
       {
-        for(current_column = 0;
-            current_column <= NUMBER_OF_COLUMNS + 1 + 2;
-            current_column++)
+        for(col = 0; col <= NUM_COLS + 1 + 2; col++)
         {
           printf("- ");
         }
         printf("\n");
       }
 
-      for(current_column = 0;
-          current_column <= NUMBER_OF_COLUMNS + 1; 
-          current_column++)
+      for(col = 0; col <= NUM_COLS + 1; col++)
       {
-        if(current_column == 1)
+        if(col == 1)
         {
           printf("| ");
         }
 
-        printf("%d ", current_grid[current_row]
-            [current_column]);
+        printf("%d ", current_grid[row][col]);
 
-        if(current_column == NUMBER_OF_COLUMNS)
+        if(col == NUM_COLS)
         {
           printf("| ");
         }
       }
       printf("\n");
 
-      if(current_row == NUMBER_OF_ROWS)
+      if(row == NUM_ROWS)
       {
-        for(current_column = 0;
-            current_column <= NUMBER_OF_COLUMNS + 1 + 2;
-            current_column++)
+        for(col = 0; col <= NUM_COLS + 1 + 2; col++)
         {
           printf("- ");
         }
@@ -239,89 +217,76 @@ int main(int argc, char **argv)
     }
 
     /* Determine the next grid -- for each row, do the following: */
-    for(current_row = 1; current_row <= NUMBER_OF_ROWS;
-        current_row++)
+    for(row = 1; row <= NUM_ROWS; row++)
     {
       /* For each column, spawn threads to do the following: */
-#pragma omp parallel for private(current_column, neighbor_row, neighbor_column, number_of_alive_neighbors)
-      for(current_column = 1; current_column <= NUMBER_OF_COLUMNS;
-          current_column++)
+#pragma omp parallel for private(col, neighbor_row, neighbor_column, num_alive_neighbors)
+      for(col = 1; col <= NUM_COLS; col++)
       {
         /* Initialize the count of ALIVE neighbors to 0 */
-        number_of_alive_neighbors = 0;
+        num_alive_neighbors = 0;
 
         /* For each row of the cell's neighbors, do the following: */
-        for(neighbor_row = current_row - 1;
-            neighbor_row <= current_row + 1;
-            neighbor_row++)
+        for(neighbor_row = row - 1; neighbor_row <= row + 1; neighbor_row++)
         {
           /* For each column of the cell's neighbors, do the following: */
-          for(neighbor_column = current_column - 1;
-              neighbor_column <= current_column + 1;
+          for(neighbor_column = col - 1; neighbor_column <= col + 1;
               neighbor_column++)
           {
             /* If the neighbor is not the cell itself, and the neighbor is
                ALIVE, do the following: */
-            if((neighbor_row != current_row
-                  || neighbor_column != current_column)
-                && (current_grid[neighbor_row]
-                  [neighbor_column] == ALIVE))
+            if((neighbor_row != row || neighbor_column != col) &&
+               (current_grid[neighbor_row][neighbor_column] == ALIVE))
             {
               /* Add 1 to the count of the number of ALIVE neighbors */
-              number_of_alive_neighbors++;
+              num_alive_neighbors++;
             }
           }
         }
 
         /* Apply Rule 1 of Conway's Game of Life */
-        if(number_of_alive_neighbors < 2)
+        if(num_alive_neighbors < 2)
         {
-          next_grid[current_row][current_column] = DEAD;
+          next_grid[row][col] = DEAD;
         }
 
         /* Apply Rule 2 of Conway's Game of Life */
-        if(current_grid[current_row][current_column] == ALIVE
-            && (number_of_alive_neighbors == 2
-              || number_of_alive_neighbors == 3))
+        if(current_grid[row][col] == ALIVE &&
+           (num_alive_neighbors == 2 || num_alive_neighbors == 3))
         {
-          next_grid[current_row][current_column] = ALIVE;
+          next_grid[row][col] = ALIVE;
         }
 
         /* Apply Rule 3 of Conway's Game of Life */
-        if(number_of_alive_neighbors > 3)
+        if(num_alive_neighbors > 3)
         {
-          next_grid[current_row][current_column] = DEAD;
+          next_grid[row][col] = DEAD;
         }
 
         /* Apply Rule 4 of Conway's Game of Life */
-        if(current_grid[current_row][current_column] == DEAD
-            && number_of_alive_neighbors == 3)
+        if(current_grid[row][col] == DEAD && num_alive_neighbors == 3)
         {
-          next_grid[current_row][current_column] = ALIVE;
+          next_grid[row][col] = ALIVE;
         }
       }
     }
 
     /* Spawn threads to copy the next grid into the current grid */
-    for(current_row = 1; current_row <= NUMBER_OF_ROWS;
-        current_row++)
+    for(row = 1; row <= NUM_ROWS; row++)
     {
-#pragma omp parallel for private(current_column)
-      for(current_column = 1; current_column <= NUMBER_OF_COLUMNS;
-          current_column++)
+#pragma omp parallel for private(col)
+      for(col = 1; col <= NUM_COLS; col++)
       {
-        current_grid[current_row][current_column] =
-          next_grid[current_row][current_column];
+        current_grid[row][col] = next_grid[row][col];
       }
     }
   }
 
   /* Deallocate data structures */
-  for(current_row = NUMBER_OF_ROWS + 1; current_row >= 0;
-      current_row--)
+  for(row = NUM_ROWS + 1; row >= 0; row--)
   {
-    free(next_grid[current_row]);
-    free(current_grid[current_row]);
+    free(next_grid[row]);
+    free(current_grid[row]);
   }
   free(next_grid);
   free(current_grid);
